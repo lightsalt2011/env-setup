643293b755098352a3d351bc0cd896bac469d570
lightsalt2011 <believelf2007@gmail.com>
2017-01-03 13:52:36 +0800

    add 'git clone solarized.git'

--- mac-env-setup/install_vim.sh
+++ mac-env-setup/install_vim.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #brew
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install git wget icdiff ag tree coreutils ctags vim 
+brew install git wget icdiff ag tree coreutils ctags vim  cmake
 #brew install git wget icdiff python3 you-get libav zsh ag sshfs pstree htop upx tree zsh-completions coreutils findutils binutils gawk gnu-getopt gnu-sed gnu-tar quilt 
 
 #gui
 #brew cask install virtualbox virtualbox-extension-pack thunder google-chrome qq mplayerx wiznote \
 #	lantern keka xquartz beyond-compare teamviewer iterm2 vlc alfred neteasemusic cakebrew \
 #	xtrafinder dash java
+
+# mac terminal config 
+git clone git://github.com/altercation/solarized.git
 
 #vim
 tar -xzvf vim_config.tar.gz
